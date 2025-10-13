6b70e1f1b79d4ca541217a8d10f8a24060d183e2
AngeloRafael02 <recioangelorafael@gmail.com>
2025-10-13 14:40:07 +0800

    Add SEO and CSP

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, DOCUMENT, Inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet,RouterModule, Router, NavigationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 import { MatDialog,MatDialogRef } from "@angular/material/dialog";
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -24,10 +25,27 @@ import { TaskTable } from './components/task-table/task-table';
   styleUrl: './app.scss'
 })
 export class App implements OnInit, OnDestroy {
+
   protected readonly title = signal('ToDoApp20');
 
+  constructor (
+    @Inject(DOCUMENT) private document: Document,
+    private titleService: Title,
+    private metaService: Meta
+  ) {
+
+  }
+
   ngOnInit(): void {
-    
+    const pageTitle = this.title()
+    const canonicalUrl = this.document.location.href.split('?')[0];
+    const tags: MetaDefinition[] = [
+      { name: 'description', content: 'To-Do-App in Angular 20 with SSR.' },
+      { property: 'og:title', content: pageTitle},
+      { rel: 'canonical', href: canonicalUrl } 
+    ];
+    this.titleService.setTitle(pageTitle);
+    this.metaService.addTags(tags);
   }
 
   ngOnDestroy(): void {
