937c375c9a833c950afed6d031e195c0b9c2e1ed
AngeloRafael02 <recioangelorafael@gmail.com>
2025-11-29 13:15:01 +0800

    call dropdown items to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, DOCUMENT, Inject, OnDestroy, OnInit, signal } from '@angular/core';
-import { RouterOutlet,RouterModule, Router, NavigationEnd } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
-import { MatDialog,MatDialogRef } from "@angular/material/dialog";
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -9,6 +8,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { Clock } from './components/clock/clock';
 import { TaskTable } from './components/task-table/task-table';
+import { BackendService } from './services/backend/backend';
+import { categoriesInterface, conditionInterface, threatInterface } from './interfaces/forms.interface';
+import { Observable, Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -27,14 +30,18 @@ import { TaskTable } from './components/task-table/task-table';
 export class App implements OnInit, OnDestroy {
 
   protected readonly title = signal('ToDoApp20');
+  public categories: categoriesInterface[] | null = null;
+  public statuses: conditionInterface[] | null = null;
+  public threatLevels: threatInterface[] | null = null;
+  
+  private dropdownSubscriptions: Subscription[] = [];
 
   constructor (
     @Inject(DOCUMENT) private document: Document,
     private titleService: Title,
-    private metaService: Meta
-  ) {
-
-  }
+    private metaService: Meta,
+    private backend:BackendService
+  ) {}
 
   ngOnInit(): void {
     const pageTitle = this.title()
@@ -46,9 +53,25 @@ export class App implements OnInit, OnDestroy {
     ];
     this.titleService.setTitle(pageTitle);
     this.metaService.addTags(tags);
+    
+    this.loadDropdownItem('categories', 'categories');
+    this.loadDropdownItem('Status', 'statuses');
+    this.loadDropdownItem('threat level', 'threatLevels');
   }
 
   ngOnDestroy(): void {
-    
+    this.dropdownSubscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  private loadDropdownItem<T>(serviceOption: string, propertyName: keyof App): void {
+    const itemObservable = this.backend.getDropdownItems(serviceOption) as Observable<T[]>;
+    const sub = itemObservable.subscribe({
+      next: (data: T[]) => {
+          (this[propertyName] as T[] | null) = data;
+          console.log(`${serviceOption} loaded:`, data);
+      },
+      error: (err: HttpErrorResponse) => console.error(`Error fetching ${serviceOption}:`, err)
+    });
+    this.dropdownSubscriptions.push(sub);
   }
 }
