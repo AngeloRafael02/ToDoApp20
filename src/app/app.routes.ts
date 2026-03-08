29d37ce5e4507d898de47bac502b3d5b36124a8c
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2026-03-08 20:19:43 +0800

    add wildcard redirect to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { ErrorComponent } from './components/error/error';
 export const routes: Routes = [
   { path: '', redirectTo: 'Status/All', pathMatch: 'prefix' },
   { path: 'Status/:status', loadComponent:() => import('./components/task-table/task-table').then(c => c.TaskTable) },
+  { path: '**', redirectTo: 'Error' },
   { path: 'Error', component:ErrorComponent },
 ];
